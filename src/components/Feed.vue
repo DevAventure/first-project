a729637bcec460e56fd1e8a1b7214f10d028e207
parinz1234 <parinya.ons@gmail.com>
2017-07-08 02:14:52 +0700

    add uoload image button

--- src/components/Feed.vue
+++ src/components/Feed.vue
@@ -3,7 +3,37 @@
     <div class="columns is-mobile">
       <div class="column has-text-centered">
         Feed
+
+        <input @change="uploadImage" class="photo-input" type="file" accept="image/*" id="capture" capture="camera"> 
+        <i @click="browseImage" class="fa fa-plus-circle fa-4x add-button" aria-hidden="true"></i>
       </div>
     </div>
   </div>
 </template>
+<script>
+  export default {
+    data: () => ({
+      photo: ''
+    }),
+    methods: {
+      browseImage () {
+        document.querySelector('.photo-input').click()
+      },
+      uploadImage (event) {
+        console.log(event.target.files[0])
+        console.log(typeof event.target.files[0])
+      }
+    }
+  }
+</script>
+<style lang="scss" scoped>
+  .photo-input {
+    display: none;
+  }
+  .add-button {
+    color: #ff3860;
+    position: fixed;
+    bottom: 50px;
+    right: 50px;
+  }
+</style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Vuefire from 'vuefire'
 import router from './router'
 import 'bulma/css/bulma.css'
+import 'font-awesome/css/font-awesome.min.css'
 import firebase from './services/firebase'
 import App from './App'
 Vue.config.productionTip = false
